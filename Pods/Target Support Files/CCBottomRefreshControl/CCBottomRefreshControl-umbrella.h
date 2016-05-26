4a259bc68e28bff15c2c702d4a5ae036b4f6e966
Dylan Wight <dylangwight@gmail.com>
2016-05-26 19:54:23 -0400

    Added design directory

---
+++ Pods/Target Support Files/CCBottomRefreshControl/CCBottomRefreshControl-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "UIScrollView+BottomRefreshControl.h"
+
+FOUNDATION_EXPORT double CCBottomRefreshControlVersionNumber;
+FOUNDATION_EXPORT const unsigned char CCBottomRefreshControlVersionString[];
+
