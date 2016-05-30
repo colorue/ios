720c9c58c76ada28739fa182d7715fb040cfc1fc
Dylan Wight <dylangwight@gmail.com>
2016-05-30 00:05:47 -0400

    added wall model

---
+++ MorphuApp/Wall.swift
@@ -0,0 +1,21 @@
+//
+//  Wall.swift
+//  Canvas
+//
+//  Created by Dylan Wight on 5/29/16.
+//  Copyright © 2016 Dylan Wight. All rights reserved.
+//
+
+import Foundation
+
+class Wall {
+    private var drawings = [Drawing]()
+    
+    func getDrawings() -> [Drawing] {
+        return self.drawings
+    }
+    
+    func addDrawing(drawing: Drawing) {
+        
+    }
+}
