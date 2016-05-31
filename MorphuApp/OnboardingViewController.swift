5952eeead9969b76bbf42319ffbf613e6baa2e48
Dylan Wight <dylangwight@gmail.com>
2016-05-30 23:22:46 -0400

    Version 3.1

--- MorphuApp/OnboardingViewController.swift
+++ MorphuApp/OnboardingViewController.swift
@@ -11,28 +11,30 @@ import UIKit
 
 class OnboardingViewController: UIViewController {
     
-    @IBOutlet weak var animation: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func facebookButton(sender: AnyObject) {
+        activityIndicator.startAnimating()
         API.sharedInstance.connectWithFacebook(self, callback: facebookCallback)
     }
     
     func facebookCallback(valid: Bool) {
+
         if valid {
             API.sharedInstance.checkLoggedIn(loginCallback)
         }
     }
     
     func loginCallback(loginValid: Bool) {
+        activityIndicator.stopAnimating()
         if loginValid {
             self.performSegueWithIdentifier("toMainController", sender: self)
         }
     }
-    
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return UIStatusBarStyle.LightContent;
