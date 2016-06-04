318599d2c2232dfc533afdfb2baa3653b35a7846
Dylan Wight <dylangwight@gmail.com>
2016-06-04 15:00:49 -0400

    search bar

--- SearchViewController.swift
+++ SearchViewController.swift
@@ -8,22 +8,47 @@
 
 import UIKit
 
-class SearchViewController: UITableViewController {
+class SearchViewController: UITableViewController, UISearchBarDelegate {
     
-    @IBAction func cancelButton(sender: UIBarButtonItem) {
-    }
-
+    let searchBar = UISearchBar(frame: CGRectMake(0, 0, 340, 20))
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let searchBar = UISearchBar(frame: CGRectMake(0, 0, 250, 20))
 
         searchBar.placeholder = "Search coming soon..."
+        
         let leftNavBarButton = UIBarButtonItem(customView:searchBar)
         self.navigationItem.leftBarButtonItem = leftNavBarButton
         
         tableView.tableFooterView = UIView()
         tableView.backgroundColor = backgroundColor
+        
+        searchBar.delegate = self
+    }
+    
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        
+        searchBar.frame = CGRectMake(0, 0, 340, 20)
+
+
+        self.navigationItem.setRightBarButtonItem(nil, animated: true)
+    }
+
+
+    func cancelBarButtonItemClicked(sender: UIBarButtonItem) {
+        self.searchBarCancelButtonClicked(self.searchBar)
+    }
+    
+    func searchBarShouldBeginEditing(searchBar: UISearchBar) -> Bool {
+
+        searchBar.frame = CGRectMake(0, 0, 285, 20)
+        
+        
+        let cancelSearchBarButtonItem = UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.Plain, target: self, action: #selector(SearchViewController.cancelBarButtonItemClicked(_:)))
+        self.navigationItem.setRightBarButtonItem(cancelSearchBarButtonItem, animated: true)
+        return true
     }
 }
