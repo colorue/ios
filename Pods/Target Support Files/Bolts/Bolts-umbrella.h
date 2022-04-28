567d420a3a86cea1c248f4c432f26c07d9d0551e
dgwight <dylan@otechie.com>
2022-04-27 21:53:48 -0400

    .gitignore

--- Pods/Target Support Files/Bolts/Bolts-umbrella.h
+++ Pods/Target Support Files/Bolts/Bolts-umbrella.h
@@ -1,5 +1,13 @@
 #ifdef __OBJC__
 #import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
 #endif
 
 #import "BFAppLink.h"
@@ -19,7 +27,6 @@
 #import "BFCancellationTokenRegistration.h"
 #import "BFCancellationTokenSource.h"
 #import "BFExecutor.h"
-#import "BFTask+Exceptions.h"
 #import "BFTask.h"
 #import "BFTaskCompletionSource.h"
 #import "Bolts.h"
